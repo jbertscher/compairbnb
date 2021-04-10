9422a32fb26decd8eadd919a519f70ab18fb966e
Jonathan Bertscher <jbertscher@gmail.com>
2021-04-10 11:01:56 +0200

    Created Trip class.

--- listing.py
+++ listing.py
@@ -2,25 +2,49 @@ import os
 from socket import send_fds
 import airbnb
 import json
+import pandas as pd
 from pymongo import MongoClient, mongo_client
 from pymongo.collection import Collection
 import re
 import requests
 
 # TODO:
-# 1. Figure out if want to be able to create instance of Listing only with listing_id or with only url or both?
+# + Removing listings from db
+# + How to properly write test functions in Python
+# + When to store in memory and when to read from db
 
 class Listing:
     api = airbnb.Api(randomize=True)
 
-    def __init__(self, listing_id: int, url: str, trip_id: str, listing_json: str = None, details: str = None):
+    def __init__(self, listing_id: int, url: str, trip_id: str, raw_listing_json: str = None, 
+        properties: str = None):
+
+        assert isinstance(listing_id, int)
         assert isinstance(trip_id, str)
         
         self.listing_id = listing_id
         self.url = url
         self.trip_id = trip_id
-        self.listing_json = listing_json
-        self.details = details
+        self.raw_listing_json = raw_listing_json
+        self.properties = properties
+
+    def get_and_parse_data(self):
+        self.raw_listing_json = Listing.get_raw_json(self)
+        self.properties = Listing.get_properties_from_raw_json(self)
+
+    def write_to_db(self, db: Collection) -> int:
+        '''
+        Writes Listing to DB. Returns the result retured by insert_one()  
+        '''        
+        if self.raw_listing_json:
+            record = {}
+            record['listing_id'] = int(self.listing_id)
+            record['url'] = self.url
+            record['trip_id'] = self.trip_id
+            record['raw_listing_json'] = self.raw_listing_json
+            record['properties'] = self.properties
+            result = db['listings'].insert_one(record)
+            return result
 
     @classmethod
     def create_from_id(cls, listing_id, trip_id):
@@ -29,78 +53,65 @@ class Listing:
 
     @classmethod
     def create_from_url(cls, url, trip_id):
-        listing_id = cls.extract_id(url)
+        # Strip query parameters, if there are any
+        if '?' in url:
+            url = url[:url.find('?')]  
+        listing_id = int(cls.extract_id(url))
         return cls(listing_id, url, trip_id)
 
     @classmethod
     def read_from_db(cls, listing_id, trip_id, db: Collection):
         assert isinstance(trip_id, str)
         
-        result = db['listings'].find_one({'pdp_listing_detail.id': listing_id, 'trip_id': trip_id})
-        return cls(result['pdp_listing_detail']['id'], result['url'], result['trip_id'])
-
-    def get_and_parse_data(self):
-        self.listing_json = Listing.get_json(self)
-        self.details = Listing.parse_json(self)
-
-    def write_to_db(self, db: Collection) -> int:
-        # Get listing json and write
-        listing_json = self.listing_json
-        if listing_json:
-            listing_json['trip_id'] = self.trip_id
-            listing_json['url'] = self.url[:self.url.find('?')]  # Strip query parameters
-            result = db['listings'].insert_one(listing_json)
-            return result
-        else:
-            return 1
+        result = db['listings'].find_one({'listing_id': listing_id, 'trip_id': trip_id})
+        return cls(result['listing_id'], result['url'], result['trip_id'], result['raw_listing_json'], result['properties'])
 
     @classmethod
-    def get_json(cls, listing):
+    def get_raw_json(cls, listing):
         try:
-            details = cls.api.get_listing_details(listing.listing_id)
-            return details
+            raw_listing_json = cls.api.get_listing_details(listing.listing_id)
+            return raw_listing_json
         except requests.exceptions.HTTPError as error:
             print (error)
             return None
 
     @classmethod
-    def parse_json(cls, listing):
-        pdp_listing_detail = listing.listing_json['pdp_listing_detail']
+    def get_properties_from_raw_json(cls, listing):
+        pdp_listing_detail = listing.raw_listing_json['pdp_listing_detail']
+
         try:
-            url = listing.listing_json['url']
+            url = listing.url
         except KeyError:
             url = ''
 
-        num_bed_types = json.dumps(cls.parse_beds(listing))
-
         p = re.compile('^([0-9]*)')
-        details = {
+        properties = {
             'listing_id': pdp_listing_detail['id'],
             'image': pdp_listing_detail['photos'][0]['large'],
             'url': url,
             'p3_summary_title': pdp_listing_detail['p3_summary_title'],
-            'bathroom_label': p.search(listing.listing_json['pdp_listing_detail']['bathroom_label']).group(0),
-            'bed_label': p.search(listing.listing_json['pdp_listing_detail']['bed_label']).group(0),
-            'bedroom_label': p.search(listing.listing_json['pdp_listing_detail']['bedroom_label']).group(0),
-            'guest_label': p.search(listing.listing_json['pdp_listing_detail']['guest_label']).group(0),
-            'num_bed_types': num_bed_types,
-            'p3_summary_title': listing.listing_json['pdp_listing_detail']['p3_summary_title'],
-            'p3_summary_address': listing.listing_json['pdp_listing_detail']['p3_summary_address']
+            'bathroom_label': p.search(listing.raw_listing_json['pdp_listing_detail']['bathroom_label']).group(0),
+            'bed_label': p.search(listing.raw_listing_json['pdp_listing_detail']['bed_label']).group(0),
+            'bedroom_label': p.search(listing.raw_listing_json['pdp_listing_detail']['bedroom_label']).group(0),
+            'guest_label': p.search(listing.raw_listing_json['pdp_listing_detail']['guest_label']).group(0),
+            'num_bed_types': json.dumps(cls.parse_beds(listing)),
+            'p3_summary_title': listing.raw_listing_json['pdp_listing_detail']['p3_summary_title'],
+            'p3_summary_address': listing.raw_listing_json['pdp_listing_detail']['p3_summary_address']
         }
 
-        return details     
+        return properties     
 
     @staticmethod
     def extract_id(url):
         if url:
-            re_groups = re.search('www.airbnb.com/rooms/(.*)\?', url)
+            re_groups = re.search('www.airbnb.com/rooms/([0-9]*).*', url)
             if re_groups:
                 listing_id = re_groups.group(1)
                 return listing_id
             
     @staticmethod
     def parse_beds(listing):
-        rooms = listing.listing_json['pdp_listing_detail']['listing_rooms']
+        rooms = listing.raw_listing_json['pdp_listing_detail']['listing_rooms']
         num_bed_types = {}
         if len(rooms) > 0:
             for room in rooms:
@@ -112,31 +123,72 @@ class Listing:
 
     @staticmethod
     def write_listing_from_url(url: str, trip_id: int, db: Collection) -> int:
-        # Extract listing listing_id from url
-        listing_id = Listing.extract_id(url)
+        listing = Listing.create_from_url(url, trip_id)
+        listing.get_and_parse_data()
         
-        # Get listing json and write
-        listing = Listing.get_json(listing_id)
-        if listing:
-            listing['trip_id'] = trip_id
-            listing['url'] = url[:url.find('?')]  # Strip query parameters
+        if listing.properties:
             result = db['listings'].insert_one(listing)
             return result
         else:
             return 1
 
-if __name__=='__main__':
-    # listing = Listing.create_from_id(4166953, '4')
-    listing = Listing.create_from_url('https://www.airbnb.com/rooms/45797974?source_impression_id=p3_1617906604_R7hqNuzB0UKlghw4', '4')
-    listing.get_and_parse_data()
 
+class Trip:
+    def __init__(self, trip_id):
+        self.trip_id = trip_id
+
+    def get_all_listings(self, db):
+        all_listing_records = db['listings'].find({'trip_id': self.trip_id})
+        # if all_listing_records.collection.count_documents({})>0:
+        all_listings = []
+        for listing_record in all_listing_records:
+            listing = Listing(listing_record['listing_id'], listing_record['url'], listing_record['trip_id'], 
+                listing_record['raw_listing_json'], listing_record['properties'])
+            all_listings.append(listing)
+        return all_listings
+        
+    def get_and_combine_all_listings(self, db) -> list:
+        all_listings = self.get_all_listings(db)
+        if len(all_listings) > 0:
+            all_listings_pd = []
+            for listing in all_listings:
+                listing_df = pd.DataFrame(listing.properties, index=[listing.listing_id])
+                all_listings_pd.append(listing_df)
+
+            return self.combine_listings(all_listings_pd)
+        else:
+            return pd.DataFrame()
+
+    @staticmethod
+    def combine_listings(listings: list[str]) -> pd.DataFrame:
+        if len(listings)>0:
+            return pd.concat(listings, axis=0)
+        else:
+            return pd.DataFrame()
+
+def test_populate_trips(db, method):
+    if method=='url':
+        urls = ['https://www.airbnb.com/rooms/45797974', 'https://www.airbnb.com/rooms/22023500?q=123']
+        for url in urls:
+            listing = Listing.create_from_url(url, '_test')    
+            listing.get_and_parse_data()
+            return listing.write_to_db(db)
+
+def test_get_and_combine_all_listings(db):
+    listings = Trip('_test').get_and_combine_all_listings(db)
+    return listings
+
+if __name__=='__main__':
     mongodb_uri = os.environ['MONGODB_URI']
     client = MongoClient(mongodb_uri)
     db=client['compairbnb']
 
-    listing.write_to_db(db)
+    def test2():
+        # print(Listing.read_from_db(34455224, '3', db))
+        found_listing = Listing.read_from_db(45797974, '5', db)
+        found_listing.get_and_parse_data()
+        print(found_listing.raw_listing_json)
 
-    # print(Listing.read_from_db(34455224, '3', db))
-    found_listing = Listing.read_from_db(45797974, '4', db)
-    found_listing.get_and_parse_data()
-    print(found_listing.listing_json)
+    # print(test_populate_trips(db, 'url'))
+    print(test_get_and_combine_all_listings(db))
+    print('done!')
