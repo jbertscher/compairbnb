eb7635564df0283a01866199033ad91a8ba56909
Jonathan Bertscher <jbertscher@gmail.com>
2021-08-08 14:21:36 +0200

    Set limit for maximum number of submit. Limit resets 24 hours after last successful submit.

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+from datetime import datetime
 from flask import Flask, jsonify, render_template, Response, request
 import os
 from pymongo import MongoClient
 from trip import Trip
+import compairbnb
 from typing import Tuple, Union
 
 app = Flask(__name__)
@@ -9,16 +11,42 @@ app = Flask(__name__)
 mongodb_uri = os.environ['MONGODB_URI']
 client = MongoClient(mongodb_uri)
 db = client['compairbnb']
+admin_collection = db['admin']
+
+SUBMIT_LIMIT = 30
 
 
 @app.route('/submit_url/<trip_id>', methods=['POST'])
 def submit_url(trip_id: str) -> Tuple[str, int]:
-    new_url = request.form.get('url')
-    if new_url != '':
-        Trip(trip_id, db).write_listing_from_url(new_url)
+    last_submit_hours_ago = (
+        datetime.now() -
+        compairbnb.read_last_submit_datetime(admin_collection)
+    ).total_seconds()/3600
+
+    print(f'Last submit was {last_submit_hours_ago} hours ago.')
+
+    if last_submit_hours_ago >= 24:
+        compairbnb.reset_submit_count()
+        print('Last submit was more than 24 hours ago. Number of submits '
+              'reset.')
+    else:
+        remaining_submits = (SUBMIT_LIMIT -
+                             compairbnb.read_submit_count(admin_collection))
+        print('Last submit was less than 24 hours ago. Number of submits '
+              f'left: {remaining_submits}.')
+
+    if(compairbnb.read_submit_count(admin_collection) >= SUBMIT_LIMIT):
+        print('Submit count exceeded. Will reset in 24 hour after last '
+              'successful submit.')
         return 'OK', 200
     else:
-        return 'ERROR', 204
+        new_url = request.form.get('url')
+        if new_url != '':
+            Trip(trip_id, db).write_listing_from_url(new_url)
+            compairbnb.increment_submit_count(admin_collection)
+            return 'OK', 200
+        else:
+            return 'ERROR', 204
 
 
 @app.route('/api/<trip_id>', methods=['GET', 'POST'])
